9ead087970a6cc4769e0614528d6b406bb7bb443
Martin <martin0207mfh@163.com>
2016-11-24 19:35:46 +0800

    sfjldskjfalkdfjlsafd

---
+++ app/src/main/java/com/phone1000/ceshi/Model.java
@@ -0,0 +1,17 @@
+package com.phone1000.ceshi;
+
+/**
+ * Created by my on 2016/11/24.
+ */
+public class Model {
+
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
